0d81ded79e9afc14f0e9e0d61e595b6bc0d2857d
illia goncharov <goncharovphp@gmail.com>
2026-03-16 13:59:53 -0700

    some changes

--- pkg/runner/gateway.go
+++ pkg/runner/gateway.go
@@ -2,6 +2,8 @@ package gateway
 
 import (
 	"fmt"
+	"os/exec"
+	"strings"
 )
 
 type Payload struct {
@@ -9,16 +11,31 @@ type Payload struct {
 	Lang string `json:"lang"`
 }
 
+var images = map[string]string{
+		"go": "golang:1.22",
+		"pythob": "python:3.12",
+		"node": "node:20",
+	}
+
 func Run(data Payload) []byte {
+
+	image := ""
 
 	switch data.Lang {
 	case "go":
-		fmt.Println("Go lang")
-		return []byte("Go lang")
+		image = images[data.Lang]
+		fmt.Println("Go lang\n" + string(data.Code))
+		return []byte("Go lang\n" + string(data.Code))
 	case "python":
-		fmt.Println("Python lang")
-		return []byte("Python lang")
+		image = images[data.Lang]
+		fmt.Println("Python lang\n" + string(data.Code))
+		return []byte("Python lang\n" + string(data.Code))
 	}
-  return []byte("Undefined lang")
+  return []byte("Undefined lang\n" + string(data.Code) + image)
+}
+
+func PythonRuntime(code string) {
+	cmd := exec.Command("docker", "run", "--rm", "-i", "python:3.12", "python")
+	cmd.Stdin = strings.NewReader(code)
 }
 
